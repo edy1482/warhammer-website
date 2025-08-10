09842accd8e5b21951a8d0e801106606482e4115
edy1482 <edward@ritchotte.com>
2025-08-10 11:15:52 -0700

    added custom admin models

--- back-end/website/army_app/admin.py
+++ back-end/website/army_app/admin.py
@@ -1,20 +1,66 @@
 from django.contrib import admin
-from .models import KeyWord, Faction, Stratagem, Detachment, Unit, UnitPointBracket, Enhancement, DataSheet, ArmyList, ArmyListEntry
+from .models import KeyWord, Faction, Detachment, Stratagem, Unit, UnitPointBracket, Enhancement, DataSheet, ArmyList, ArmyListEntry
 # Register your models here.
 
-admin.site.register(KeyWord)
-admin.site.register(Faction)
-admin.site.register(Detachment)
-admin.site.register(Stratagem)
-admin.site.register(Unit)
-admin.site.register(UnitPointBracket)
-admin.site.register(Enhancement)
-admin.site.register(DataSheet)
-admin.site.register(ArmyList)
+@admin.register(KeyWord)
+class KeyWordAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    
+@admin.register(Faction)
+class FactionAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+@admin.register(Detachment)
+class DetachmentAdmin(admin.ModelAdmin):
+    list_display = ("name", "faction")
+    search_fields = ("name",)
+    list_filter = ("faction",)
+
+@admin.register(Stratagem)
+class StratagemAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    list_filter = ("detachment",)
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ("name", "faction")
+    search_fields = ("name",)
+    list_filter = ("faction", "keywords",)
+    filter_horizontal = ("keywords",)
+    ordering = ("name",)
+
+@admin.register(UnitPointBracket)
+class UnitPointBracketAdmin(admin.ModelAdmin):
+    list_display = ("unit", "points")
+    search_fields = ("unit__name",) # Search fields expect columns, so double underscore
+    list_filter = ("unit__faction",)
+    
+@admin.register(Enhancement)
+class EnhancementAdmin(admin.ModelAdmin):
+    list_display = ("name", "detachment", "points",)
+    search_fields = ("name",)
+    list_filter = ("detachment__faction",)
+
+@admin.register(DataSheet)
+class DataSheetAdmin(admin.ModelAdmin):
+    list_display = ("unit", "upload_file", "source")
+    search_fields = ("unit__name",)
+    list_filter = ("unit__faction",)
+
+@admin.register(ArmyList)
+class ArmyListAdmin(admin.ModelAdmin):
+    list_display = ("user", "name", "faction", "detachment")
+    search_fields = ("name",)
+    list_filter = ("faction", "detachment")
 
 @admin.register(ArmyListEntry)
 class ArmyListEntryAdmin(admin.ModelAdmin):
-    list_display = ("army_list", "unit", "model_count", "enhancement", "is_warlord" ,"available_stratagems")
+    list_display = ("army_list", "unit", "model_count", "points","enhancement", "is_warlord" ,"available_stratagems")
+    list_filter = ("army_list", "unit")
+
+    def points(self, obj):
+        return obj.get_total_points()
 
     def available_stratagems(self, obj):
         return ", ".join(strat.name for strat in obj.get_valid_strats())

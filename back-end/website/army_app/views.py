579cd94ec07e2ac7841356dd4cf361c68ca355d8
edy1482 <edward@ritchotte.com>
2025-11-05 22:13:39 -0600

    added faction view and html

--- back-end/website/army_app/views.py
+++ back-end/website/army_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from army_app.models import Faction
+from django.http import Http404
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def factions(request):
+    try:
+        all_factions = Faction.objects.all()
+    except:
+        raise Http404("Factions not found")
+    return render(request, 'factions.html', {"all_factions" : all_factions})

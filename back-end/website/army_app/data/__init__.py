3304669792567bedd1b27514f4b27b70d980c0c9
edy1482 <edward@ritchotte.com>
2025-11-22 07:36:34 -0600

    added ability effects to init

--- back-end/website/army_app/data/__init__.py
+++ back-end/website/army_app/data/__init__.py
@@ -1,11 +1,11 @@
-from .data_loaders import load_factions, load_detachments, load_enhancements, load_stratagems, load_abilities
+from .data_loaders import load_factions, load_detachments, load_enhancements, load_stratagems, load_abilities, load_ability_effects
 from .data_loaders import load_weapons
-from .data_loaders import load_units, load_unit_point_brackets, load_data_sheet
+from .data_loaders import load_units, load_unit_point_brackets
 from .data_loaders import load_leadership
 
 __all__ = [
-    "load_abilities", "load_factions", "load_detachments", "load_enhancements", "load_stratagems",
+    "load_abilities", "load_ability_effects", "load_factions", "load_detachments", "load_enhancements", "load_stratagems",
     "load_weapons",
-    "load_units", "load_unit_point_brackets", "load_data_sheet",
+    "load_units", "load_unit_point_brackets",
     "load_leadership",
 ]
